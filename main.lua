894f5d6f2f7c339f56b6a980785c15d10feeaebc
Babak Jahedmanesh <babak2000ir@gmail.com>
2025-08-24 14:25:03 +0100

    Random movement - more state management

--- main.lua
+++ main.lua
@@ -2,45 +2,96 @@ if arg[2] == "debug" then
    require("lldebugger").start()
 end
 
+local tick
 local ninja = {}
+local head = {}
 local lastKeyMessage = ""
 
 function love.load()
-   ninja.image = love.graphics.newImage("assets/Characters/NinjaFrog/Fall (32x32).png")
+   tick = require "tick"
+
+   tick.recur(function()
+      head.movex = math.random(-1, 1)
+      head.movey = math.random(-1, 1)
+   end, 1)
+
+   ninja.image = love.graphics.newImage("assets/characters/NinjaFrog/Fall (32x32).png")
    ninja.imgx = 100 -- starting X position
    ninja.imgy = 100 -- starting Y position
    ninja.scale = 4  -- scale factor for the image
    ninja.speed = 100
+
+   head.image = love.graphics.newImage("assets/scenery/Rock Head/Idle.png")
+   head.imgx = 300 -- starting X position
+   head.imgy = 100 -- starting Y position
+   head.scale = 5  -- scale factor for the image
+   head.speed = 10
+   head.movex = 0
+   head.movey = 0
+
    love.graphics.setNewFont(18)
    love.graphics.setBackgroundColor(255, 255, 255)
 end
 
+local function canmovex(sprite, distance)
+   if distance < 0 and sprite.imgx > 0 then
+      return true
+   end
+   if distance > 0 and sprite.imgx < love.graphics.getWidth() - sprite.image:getWidth() * sprite.scale then
+      return true
+   end
+   return false
+end
+
+local function canmovey(sprite, distance)
+   if distance < 0 and sprite.imgy > 0 then
+      return true
+   end
+   if distance > 0 and sprite.imgy < love.graphics.getHeight() - sprite.image:getHeight() * sprite.scale then
+      return true
+   end
+   return false
+end
+
+local function movex(sprite, distance, dt)
+   if canmovex(sprite, distance) then
+      sprite.imgx = sprite.imgx + sprite.speed * sprite.scale * dt * distance
+   end
+end
+
+local function movey(sprite, distance, dt)
+   if canmovey(sprite, distance) then
+      sprite.imgy = sprite.imgy + sprite.speed * sprite.scale * dt * distance
+   end
+end
+
 function love.update(dt)
-   if ninja.imgx > 0 then
-      if love.keyboard.isDown("left") then
-         ninja.imgx = ninja.imgx - ninja.speed * ninja.scale * dt
-      end
+   tick.update(dt)
+
+   if love.keyboard.isDown("left") then
+      movex(ninja, -1, dt)
    end
-   if ninja.imgx < love.graphics.getWidth() - ninja.image:getWidth() * ninja.scale then
-      if love.keyboard.isDown("right") then
-         ninja.imgx = ninja.imgx + ninja.speed * ninja.scale * dt
-      end
+
+   if love.keyboard.isDown("right") then
+      movex(ninja, 1, dt)
    end
-   if ninja.imgy > 0 then
-      if love.keyboard.isDown("up") then
-         ninja.imgy = ninja.imgy - ninja.speed * ninja.scale * dt
-      end
+
+   if love.keyboard.isDown("up") then
+      movey(ninja, -1, dt)
    end
-   if ninja.imgy < love.graphics.getHeight() - ninja.image:getHeight() * ninja.scale then
-      if love.keyboard.isDown("down") then
-         ninja.imgy = ninja.imgy + ninja.speed * ninja.scale * dt
-      end
-   end;
+
+   if love.keyboard.isDown("down") then
+      movey(ninja, 1, dt)
+   end
+
+   movex(head, head.movex, dt)
+   movey(head, head.movey, dt)
 end
 
 function love.draw()
    love.graphics.setColor(1, 1, 1, 1)                                                   -- Reset to white (no tinting)
    love.graphics.draw(ninja.image, ninja.imgx, ninja.imgy, 0, ninja.scale, ninja.scale) -- draw image at imgx, imgy with no rotation and scaled to 2x size
+   love.graphics.draw(head.image, head.imgx, head.imgy, 0, head.scale, head.scale)      -- draw image at imgx, imgy with no rotation and scaled to 2x size
    love.graphics.setColor(0, 0, 0)
    love.graphics.print("Click and drag ninja-frog around or use the arrow keys", 10, 10)
    love.graphics.print(lastKeyMessage, 10, 30)
